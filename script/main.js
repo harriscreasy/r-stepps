30fe1dded25d3453119824231be74f7c2542b7b2
Harris Zaoui <harriszaoui@MacBook-Air-de-Harris.local>
2021-01-08 14:19:42 +0100

    Premiere version

--- script/main.js
+++ script/main.js
@@ -4,3 +4,11 @@ const container = document.querySelector(".container");
 hamburger_menu.addEventListener("click", () => {
   container.classList.toggle("active");
 });
+
+
+
+const banner = document.querySelector(".bannerVid");
+window.addEventListener("scroll", function(){
+    let value = window.scrollY;
+    banner.style.clipPath = "circle("+ value +"px at center center)";
+})
