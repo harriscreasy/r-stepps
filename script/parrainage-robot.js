c3c07158e8bece60d2f7c1db9e41abec9398a319
Harris Zaoui <harriszaoui@macbook-air-de-harris.home>
2021-01-18 12:19:12 +0100

    Ajout de parrainage

---
+++ script/parrainage-robot.js
@@ -0,0 +1,65 @@
+var s = skrollr.init();
+
+function toggle(){
+    let close = document.querySelector(".close");
+    let trailer = document.querySelector(".trailer-video");
+    trailer.classList.toggle("active");
+    
+  }
+
+  
+
+  function toggleimage(){
+    let close = document.querySelector(".close");
+    let trailer = document.querySelector(".trailer-image");
+    let trailerimage = document.querySelector(".trailer-image img");
+    trailer.classList.toggle("active");
+    trailerimage.src = "/img/info-robot-image/presentation-digger.png" ;
+    
+  }
+
+  function toggleimagetracker(){
+    let close = document.querySelector(".close");
+    let trailer = document.querySelector(".trailer-image");
+    let trailerimage = document.querySelector(".trailer-image img");
+    trailer.classList.toggle("active");
+    trailerimage.src = "/img/tracker-robot.png" ;
+    
+  }
+
+  function toggleimagedigger(){
+    let close = document.querySelector(".close");
+    let trailer = document.querySelector(".trailer-image");
+    let trailerimage = document.querySelector(".trailer-image img");
+    trailer.classList.toggle("active");
+    trailerimage.src = "/img/digger-robot.png" ;
+    
+  }
+
+  function toggleimagecarrier(){
+    let close = document.querySelector(".close");
+    let trailer = document.querySelector(".trailer-image");
+    let trailerimage = document.querySelector(".trailer-image img");
+    trailer.classList.toggle("active");
+    trailerimage.src = "/img/hydro-robot.png";
+    
+  }
+
+  function toggleimagehydro(){
+    let close = document.querySelector(".close");
+    let trailer = document.querySelector(".trailer-image");
+    let trailerimage = document.querySelector(".trailer-image img");
+    trailer.classList.toggle("active");
+    trailerimage.src = "/img/carrier-png.png";
+    
+  }
+
+
+  function toggleimagehydro(){
+    let close = document.querySelector(".close");
+    let trailer = document.querySelector(".trailer-image");
+    let trailerimage = document.querySelector(".trailer-image img");
+    trailer.classList.toggle("active");
+    trailerimage.src = "/img/carrier-png.png";
+    
+  }
